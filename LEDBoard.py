d5163dabc4fae7d602eeed3160a66011e9b846c2
pythoncoder8 <pycoder8@gmail.com>
2021-04-18 14:15:56 -0400

    Reupload LEDBoard.py

--- LEDBoard.py
+++ LEDBoard.py
@@ -1,12 +1,63 @@
 from gpiozero import LEDBoard
 from time import sleep
 from signal import pause
+import logging
 
-leds = LEDBoard(6, 5, 13, pwm=True)
-while True:
-    leds.value = (1, 1, 1)
-    sleep(1)
-    leds.value = (0.5, 0.5, 0.5)
-    sleep(1)
-    leds.value = (0, 0, 0)
-    sleep(1)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('app.log', mode='w')
+file_handler.setLevel(logging.DEBUG)
+logger.addHandler(file_handler)
+
+print('=================================================')
+print('|                   LEDBoard                    |')
+print('|  -------------------------------------------  |')
+print('|            Use 220 ohm resistors              |')
+print('|         Connect LED cathode to GND            |')
+print('|         Uses pulse-width modulation           |')
+print('|        Click Ctrl + C to exit program         |')
+print('|------------------------------------------------')
+print('|                                   PythonCoder8|')
+print('=================================================')
+
+try:
+    leds = LEDBoard(5, 6, 13, pwm=True)
+    while True:
+        leds.value = (1, 1, 1)
+        sleep(0.03)
+
+        leds.value = (0.9, 0.9, 0.9)
+        sleep(0.03)
+
+        leds.value = (0.8, 0.8, 0.8)
+        sleep(0.03)
+
+        leds.value = (0.7, 0.7, 0.7)
+        sleep(0.03)
+
+        leds.value = (0.6, 0.6, 0.6)
+        sleep(0.03)
+
+        leds.value = (0.5, 0.5, 0.5)
+        sleep(0.03)
+
+        leds.value = (0.4, 0.4, 0.4)
+        sleep(0.03)
+
+        leds.value = (0.3, 0.3, 0.3)
+        sleep(0.03)
+
+        leds.value = (0.2, 0.2, 0.2)
+        sleep(0.03)
+
+        leds.value = (0.1, 0.1, 0.1)
+        sleep(0.03)
+
+        leds.value = (0, 0, 0)
+        sleep(0.75)
+
+except KeyboardInterrupt:
+    print('\nExiting program...')
+
+except Exception as exception:
+    logger.exception('Encountered unhandled exception\n %s', exception)
